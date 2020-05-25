a89105c4fe58af9d978745d2431401234b9d69ea
Bndgld <glad_adiwijaya@yahoo.co.id>
2020-05-26 02:14:56 +0700

    benerin hewan dari lord

--- application/controllers/Hewan.php
+++ application/controllers/Hewan.php
@@ -13,38 +13,83 @@ Class Hewan extends REST_Controller{
     }
 
     public function index_get(){
+        return $this->returnData($this->db->get_where('hewans', ["aktif" => 1])->result(), false);
+    }
+
+    public function nonAktif_get(){
+        return $this->returnData($this->db->get_where('hewans', ["aktif" => 0])->result(), false);
+    }
+
+    public function all_get(){
         return $this->returnData($this->db->get('hewans')->result(), false);
+    }
+
+    public function getWithJoin_get(){
+        $this->db->select('id_hewan, hewans.id_customer, customers.nama_customer "nama customer", customers.alamat_customer "alamat customer", 
+                        customers.tglLahir_customer "tanggal lahir customer", customers.noTelp_customer "telepon customers",
+                        hewans.id_jenisHewan, jenishewans.nama "nama_jenis_hewan", hewans.nama_hewan "nama hewan", hewans.tglLahir_hewan "tanggal lahir hewan", 
+                        hewans.createLog_at, hewans.createLog_by, hewans.updateLog_at, hewans.deleteLog_at, hewans.aktif');
+        $this->db->from('hewans');
+        $this->db->join('customers', 'hewans.id_customer = customers.id_customer');
+        $this->db->join('jenishewans', 'hewans.id_jenisHewan = jenishewans.id_jenisHewan');
+        $this->db->where('hewans.aktif',1);
+        $this->db->order_by('hewans.id_hewan ASC');
+        return $this->returnData($this->db->get()->result(), false);
+    }
+
+    public function getAllWithJoin_get(){
+        $this->db->select('id_hewan, hewans.id_customer, customers.nama_customers "nama customer", customers.alamat_customer "alamat customer", 
+                        customers.tglLahir_customer "tanggal lahir customer", customers.noTelp_customer "telepon customer",
+                        hewans.id_jenisHewan, jenishewans.nama_jenisHewan "nama jenis hewan", hewans.nama_hewan "nama hewan", hewan.tglLahir_hewan "tanggal lahir hewan", 
+                        hewans.createLog_at, hewans.createLog_by, hewans.updateLog_at, hewans.deleteLog_at, hewans.aktif');
+        $this->db->from('hewans');
+        $this->db->join('customers', 'hewans.id_customer = customers.id_customer');
+        $this->db->join('jenishewans', 'hewans.id_jenisHewan = jenishewans.id_jenisHewan');
+        $this->db->order_by('hewans.id_hewan ASC');
+        return $this->returnData($this->db->get()->result(), false);
+    }
+
+    public function search_get($id_hewan){
+        return $this->returnData($this->db->get_where('hewans', ["id_hewan" => $i_hewan])->row(), false);
     }
 
     public function index_post($id_hewan = null){
         $validation = $this->form_validation;
         $rule = $this->HewanModel->rules();
-        /*if($id == null){
-            array_push($rule, [
-                'field' => 'password',
-                'label' => 'password',
-                'rules' => 'required'
-            ],
-            [
-                'field' => 'email',
-                'label' => 'email',
-                'rules' => 'required|valid_email|is_unique[users.email]'
-            ]);
+        if($id_hewan == null){
+            array_push($rule,
+                [
+                    'field' => 'nama_hewan',
+                    'label' => 'nama_hewan',
+                    'rules' => 'required'
+                ],
+                [
+                    'field' => 'tglLahir_hewan',
+                    'label' => 'tglLahir_hewan',
+                    'rules' => 'required'
+                ],
+                [
+                    'field' => 'id_customer',
+                    'label' => 'id_customer',
+                    'rules' => 'required'
+                ],
+                [
+                    'field' => 'id_jenisHewan',
+                    'label' => 'id_jenisHewan',
+                    'rules' => 'required'
+                ],
+                [
+                    'field' => 'updateLog_by',
+                    'label' => 'updateLog_by',
+                    'rules' => 'required'
+                ]
+            );
         }
-        else{
-            array_push($rule, 
-            [
-                'field' => 'email',
-                'label' => 'email',
-                'rules' => 'required|valid_email'
-            ]);
-            }*/
         $validation->set_rules($rule);
         if(!$validation->run()){
             return $this->returnData($this->form_validation->error_array(), true);
         }
         $user = new HewanData();
-        // $user->id_hewan = $this->post('id_hewan');
         $user->nama_hewan = $this->post('nama_hewan');
         $user->tglLahir_hewan = $this->post('tglLahir_hewan');
         $user->id_customer = $this->post('id_customer');
@@ -59,14 +104,75 @@ Class Hewan extends REST_Controller{
         return $this->returnData($response['msg'], $response['error']);
     }
 
-
-    public function index_delete($id_hewan = null){
-        if($id_hewan == null){
-            return $this->returnData('Parameter Id Tidak Ditemukan', true);
+    public function update_post($id_hewan = null){
+        $validation = $this->form_validation;
+        $rule = $this->HewanModel->rules();
+        if($id_hewan != null){
+            array_push($rule,
+            [
+                'field' => 'nama_hewan',
+                'label' => 'nama_hewan',
+                'rules' => 'required'
+            ],
+            [
+                'field' => 'tglLahirHewan',
+                'label' => 'tglLahirHewan',
+                'rules' => 'required'
+            ],
+            [
+                'field' => 'id_customer',
+                'label' => 'id_customer',
+                'rules' => 'required'
+            ],
+            [
+                'field' => 'id_jenisHewan',
+                'label' => 'id_jenisHewan',
+                'rules' => 'required'
+            ],
+            [
+                'field' => 'updateLog_by',
+                'label' => 'updateLog_by',
+                'rules' => 'required'
+            ]
+            );
         }
-        $response = $this->HewanModel->destroy($id_hewan);
+        $validation->set_rules($rule);
+        if (!$validation->run()) {
+            return $this->returnData($this->form_validation->error_array(), true);
+        }
+        $user = new userData();
+        $user->nama_hewan = $this->post('nama_hewan');
+        $user->tglLahir_hewan = $this->post('tglLahir_hewan');
+        $user->id_customer = $this->post('id_customer');
+        $user->id_jenisHewan = $this->post('id_jenisHewan');
+        $user->updateLog_by = $this->post('updateLog_by');
+        if($id_hewan != null){
+            $response = $this->userModel->update($user,$id_hewan);
+        }
         return $this->returnData($response['msg'], $response['error']);
     }
+
+    public function delete_post($id_hewaan = null){
+        $validation = $this->form_validation;
+        $rule = $this->HewanModel->rules();
+        $validation->set_rules($rule);
+        if (!$validation->run()) {
+            return $this->returnData($this->form_validation->error_array(), true);
+        }
+        $hewan = new HewanData();
+        if($id_hewan != null){
+            $response = $this->HewanModel->softDelete($hewan,$id_hewan);
+        }
+        return $this->returnData($response['msg'], $response['error']);
+    }
+
+    // public function index_delete($id_hewan = null){
+    //     if($id_hewan == null){
+    //         return $this->returnData('Parameter Id Tidak Ditemukan', true);
+    //     }
+    //     $response = $this->HewanModel->destroy($id_hewan);
+    //     return $this->returnData($response['msg'], $response['error']);
+    // }
 
     public function returnData($msg, $error){
         $response['error'] = $error;
@@ -85,4 +191,5 @@ Class HewanData{
     public $createLog_at;
     public $updateLog_at;
     public $deleteLog_at;
+    public $aktif;
 }
