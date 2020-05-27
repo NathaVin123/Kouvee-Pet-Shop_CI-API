490714d2483a3ec00af3109c1d2c80f0e720c71b
Nathaniel Vincent Wibowo <170709127@students.uajy.ac.id>
2020-05-27 12:04:41 +0700

    dawdd

--- application/controllers/Pengadaan.php
+++ application/controllers/Pengadaan.php
@@ -100,7 +100,7 @@ Class Pengadaan extends REST_Controller{
                 'field' => 'id_supplier',
                 'label' => 'id_supplier',
                 'rules' => 'required'
-            ],
+            ]
         );
         $validation->set_rules($rule);
         if(!$validation->run()){
